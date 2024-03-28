5f267ce26458da9e134bf10ce30877f10aa4c223
Jiayang Li <admin@0x10c.pw>
2024-03-27 20:10:02 -0400

    fix bugs in the mkgen

--- SimEnvControl/user_cmd/mkgen.py
+++ SimEnvControl/user_cmd/mkgen.py
@@ -5,7 +5,7 @@ import click
 
 from ..libsimenv.app_manifest import Manifest_t, verify_manifest_format
 from ..libsimenv.autocomplete import complete_chkpt_names, complete_app_names
-from ..libsimenv.checkpoints_db import check_checkpoint_exist
+from ..libsimenv.checkpoints_db import check_checkpoint_exist, get_checkpoint_abspath
 from ..libsimenv.manifest_db import load_from_manifest_db, prompt_app_name_suggestion
 from ..libsimenv.repo_path import get_repo_components_path
 from ..libsimenv.shcmd_utils import add_base_to_stdin_file_in_shcmd
@@ -19,7 +19,7 @@ BOOTSTRAP_SIMULATOR = "spike"
 
 def mkgen_bootstrap(manifest, repo_path):
     # type: (Manifest_t, str) -> str
-    actual_app_cmd = add_base_to_stdin_file_in_shcmd(manifest["app_init_cwd"], "$(SIMENV_SYSROOT)", "$(APP_INIT_CWD)")
+    actual_app_cmd = add_base_to_stdin_file_in_shcmd(manifest["app_cmd"], "$(SIMENV_SYSROOT)", "$(APP_INIT_CWD)")
     bootstrap_recipes = instantiate_template(
         "bootstrap-make-recipes",
         repo_path=repo_path
@@ -42,12 +42,12 @@ def mkgen_bootstrap(manifest, repo_path):
 
 def mkgen_normal(manifest, checkpoint_to_load_path):
     # type: (Manifest_t, str) -> str
-    actual_app_cmd = add_base_to_stdin_file_in_shcmd(manifest["app_init_cwd"], "$(SIMENV_SYSROOT)", "$(APP_INIT_CWD)")
+    actual_app_cmd = add_base_to_stdin_file_in_shcmd(manifest["app_cmd"], "$(SIMENV_SYSROOT)", "$(APP_INIT_CWD)")
     actual_ckpt_flag = os.getenv("RISCV_SIMENV_SIM_FLAG_LDCKPT", default=DEFAULT_CHECKPOINT_LOAD_FLAG)
     actual_sim = os.getenv("RISCV_SIMENV_SIM_CMD", default=DEFAULT_SIMULATOR)
 
     if checkpoint_to_load_path:
-        sim_flags = f"{actual_ckpt_flag} \"{checkpoint_to_load_path}\""
+        sim_flags = f"{actual_ckpt_flag}\"{checkpoint_to_load_path}\""
     else:
         sim_flags = ""
     generated_makefile = instantiate_template(
@@ -100,7 +100,9 @@ def cmd_mkgen(ctx, app_name, checkpoint, bootstrap):
         if bootstrap:
             generated_makefile = mkgen_bootstrap(manifest, ctx.obj["repo_path"])
         else:
-            generated_makefile = mkgen_normal(manifest, checkpoint)
+            checkpoint_path = get_checkpoint_abspath(checkpoints_archive_path, app_name,
+                                                     checkpoint) if checkpoint else None
+            generated_makefile = mkgen_normal(manifest, checkpoint_path)
 
         with open("Makefile", "w") as fp:
             fp.write(generated_makefile)
